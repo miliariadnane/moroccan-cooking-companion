65c10d71b08c74b57ea047bb0ecc37dd50318e30
Adnane Miliari <adnane.miliari@transperfect.com>
2024-04-03 22:41:27 +0000

    Bismillah 🐣

---
+++ src/main/java/dev/nano/mcc/MCCApplication.java
@@ -0,0 +1,13 @@
+package dev.nano.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MCCApplication.class, args);
+    }
+
+}
