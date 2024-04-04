2bb8ab05828315be6cc1355b8ec6e12c201d7d36
Adnane Miliari <adnane.miliari@transperfect.com>
2024-04-04 17:11:29 +0000

    :necktie: add  Vaadin's BrowserCallable and AnonymousAllowed annotations

--- src/main/java/dev/nano/mcc/MCCService.java
+++ src/main/java/dev/nano/mcc/MCCService.java
@@ -1,7 +1,23 @@
 package dev.nano.mcc;
 
-import org.springframework.stereotype.Service;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.BrowserCallable;
 
-@Service
+@BrowserCallable
+@AnonymousAllowed
 public class MCCService {
+
+    private final MCCAssistant mccAssistant;
+
+    public MCCService(MCCAssistant mccAssistant) {
+        this.mccAssistant = mccAssistant;
+    }
+
+    public String getRecipes(String dishName) {
+        return mccAssistant.getRecipes(dishName);
+    }
+
+    public String getDishImage(String dishName) {
+        return mccAssistant.getDishImage(dishName);
+    }
 }
